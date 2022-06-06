17f4aa1dbce853c75e4cc77f7196de6feccaa81a
Mike Liang <mikeliang@Mikes-MacBook-Air.local>
2022-06-06 12:41:16 -0400

    finish boilerplate express app

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+
+const API_ROUTE = '/api/v0';
+
+const app = express();
+
+const corsOptions = require('./config/cors.config');
+
+app.use(cors(corsOptions));
+app.use(helmet());
+app.use(compression());
+app.use(bodyParser());
+app.disable('x-powered-by');
+
+// listening to the port
+const PORT = 8000;
+app.listen(PORT, () => console.log(`listening on port ${PORT}`)); // dev
+
+// index
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
---
+++ config/cors.config.js
@@ -0,0 +1,7 @@
+const corsOptions = {
+  origin: 'http://localhost:8000', // dev, remove for production
+  // production
+  // origin: '<some domain>',
+};
+
+module.exports = corsOptions;
