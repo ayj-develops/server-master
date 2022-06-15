420598c1a60f404150e1b5455c532b6c0a5daa19
Mike Liang <mikeliang@Mikes-MacBook-Air.local>
2022-06-15 09:39:15 -0400

    add slug field

--- models/club.model.js
+++ models/club.model.js
@@ -1,34 +1,38 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const ClubSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    description: {
-        type: String,
-        required: true,
-        unique: false,
-        minlength: 1,
-        maxlength: 100
-    },
-    execs: {
-        type: Array,
-        required: true,
-        unique: false,
-        minlength: 1
-    },
-    teacher: {
-        type: Array,
-        required: true,
-        unique: false,
-        minlength: 1
-    }
-})
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  slug: {
+    type: String,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    unique: false,
+    minlength: 1,
+    maxlength: 100,
+  },
+  execs: {
+    type: Array,
+    required: true,
+    unique: false,
+    minlength: 1,
+  },
+  teacher: {
+    type: Array,
+    required: true,
+    unique: false,
+    minlength: 1,
+  },
+});
 
-const Club = mongoose.model("Club", ClubSchema);
+const Club = mongoose.model('Club', ClubSchema);
 
 module.exports = Club;
