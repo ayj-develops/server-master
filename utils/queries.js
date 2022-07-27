a99fd76ec90f66f7cec1d19774b265b60441dda1
Mike Liang <liang.mike.to@gmail.com>
2022-07-26 21:19:22 -0400

    move helper functions to utils

---
+++ utils/queries.js
@@ -0,0 +1,38 @@
+const User = require('../models/user.model');
+const Club = require('../models/club.model');
+const Comment = require('../models/comment.model');
+const Post = require('../models/post.model');
+
+const { checkExist } = require('./exist');
+
+const getUser = async (field, param) => User.findOne({ [field]: param }, async (err, user) => {
+  if (err) return { code: 500, Message: err };
+  if (!checkExist(user)) return { code: 404, Message: 'User not found' };
+  return { user };
+}).clone();
+
+const getClub = async (field, param) => Club.findOne({ [field]: param }, async (err, club) => {
+  if (err) return { code: 500, Message: err };
+  if (!checkExist(club)) return { code: 404, Message: 'Club not found' };
+  return club;
+}).clone();
+
+const getComment = async (field, param) => Comment.findOne(
+  { [field]: param },
+  async (err, comment) => {
+    if (err) return { code: 500, Message: err };
+    if (!checkExist(comment)) return { code: 404, Message: 'Comment not found' };
+    return comment;
+  },
+).clone();
+
+const getPost = async (field, param) => Post.findOne({ [field]: param }, async (err, post) => {
+  if (err) return { code: 500, Message: err };
+  if (!checkExist(post)) return { code: 404, Message: 'User not found' };
+  return { post };
+}).clone();
+
+exports.getUser = getUser;
+exports.getClub = getClub;
+exports.getComment = getComment;
+exports.getPost = getPost;
